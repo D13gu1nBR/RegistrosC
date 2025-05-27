9755272623a0353dbf7c030f8e0ba1eb1162f868
D13gu1nBR <109635184+D13gu1nBR@users.noreply.github.com>
2025-05-27 16:01:39 +0000

    .commit

---
+++ Questao2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct Produto{
+    int codigo;
+    char descricao[500];
+    double valorUnitario;
+    int quantidade;
+};
+
+int cadastro(struct Produto produtos[], int* QuantidadeCadastrada){
+    
+    }
+}
